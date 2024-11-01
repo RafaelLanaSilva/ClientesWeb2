fea8ae8234d336f506e3627e8799ec7719ee7c89
RafaelLanaSilva <rafaellanas@gmail.com>
2024-11-01 12:37:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    NgxSpinnerModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
